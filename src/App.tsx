771bd2747e10b15a395a2461351da30eec2d60f8
Robbie <yashoct568@gmail.com>
2026-02-13 23:51:18 +0530

    added vercel analytics and page view

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/context/AuthContext";
 import { SocketProvider } from "@/context/SocketProvider";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import ProtectedRoute from "@/routes/ProtectedRoute";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
@@ -25,6 +27,8 @@ const App = () => (
     <AuthProvider>
       <TooltipProvider>
         <Sonner />
+        <SpeedInsights />
+        <Analytics />
         <BrowserRouter>
           <SocketProvider>
             <Routes>
